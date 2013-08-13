1673deb0048efcd9d59d29a662b2aee9fee1c4be
BB20101997 <blessmann-bennet@t-online.de>
2013-08-13 20:42:01 +0200

    Now Shorter and cleaner! :)

--- common/bb/mods/bbbc/world/gen/structure/BrickHouse.java
+++ common/bb/mods/bbbc/world/gen/structure/BrickHouse.java
@@ -6,13 +6,24 @@ import net.minecraft.world.World;
 
 public class BrickHouse {
 	
+	static int Brick = 45;
+	static int BrickStair = 108;
+	static int Air = 0;
+	static int BookShelf = 47;
+	static int doorWood = 64;
+	static int doorIron = 71;
+	static int obsidian = 49;
+	static int glowStone = 89;
+	static int entchantTable = 116;
+	static int BrickSlab = 44;
+
 	public static void BuildBrickHouse(World par1,int par2,int par3,int par4,EntityPlayer par5,int f){
 	
 		
 		if (!Reference.DEBUGMODE){
 			switch(f){
 			case 0:{south(par1,par2,par3,par4,par5);break;}
-			case 1:{west(par1,par2-5,par3,par4-4,par5);break;}
+			case 1:{west(par1,par2,par3,par4,par5);break;}
 			case 2:{north(par1,par2,par3,par4,par5);break;}
 			case 3:{par5.addChatMessage("This feature is NIY");	break;}	
 			}
@@ -25,237 +36,127 @@ public class BrickHouse {
 	
 	public static void north(World par1,int par2,int par3,int par4,EntityPlayer par5){
 		
-		par1.setBlock(par2-1, par3, par4+1,108);
-		par1.setBlock(par2+1,par3,par4,108,1,0x01);
-		
-		for(int i = 1;i<=4;i++){par1.setBlock(par2-i,par3,par4,108,3,0x01);}
-		
-		par1.setBlock(par2,  par3,par4+1,108,3,0x01);
-		par1.setBlock(par2+1,par3,par4+1,108,3,0x01);
-		
-		for(int i = 2;i<=4;i++){par1.setBlock(par2+i,par3,par4,108,3,0x01);}
-		
-		for(int i = 1;i<=8;i++){
-		    par1.setBlock(par2+4,par3,par4-i,108,1,0x01);
-		    par1.setBlock(par2-4,par3,par4-i,108,0,0x01);}
-		
-		for(int i =-3;i<=3;i++){
-		    par1.setBlock(par2-i,par3  ,par4-8,108,2,0x01);
-		    par1.setBlock(par2-i,par3+3,par4-1,108,3,0x01);
-			par1.setBlock(par2-i,par3+3,par4-7,108,2,0x01);}
-		
-	
-		for(int i = 1;i<=7;i++){
-		    par1.setBlock(par2-3,par3+3,par4-i,108,0,0x01);
-			par1.setBlock(par2+3,par3+3,par4-i,108,1,0x01);
-			for(int ii = -3;ii<=3;ii++){
-			    for(int iii = 0;iii<=2;iii++){ par1.setBlock(par2+ii, par3+iii, par4-i, 45);}
-			    }
-			}
-		
-		for(int i = 2;i<=6;i++){
-		    par1.setBlock(par2-2,par3+4,par4-i,108,0,0x01);
-            par1.setBlock(par2+2,par3+4,par4-i,108,1,0x01); 
-			for(int ii = -2;ii<=2;ii++){
-			    for(int iii = 1;iii<=3;iii++){par1.setBlock(par2+ii, par3+iii, par4-i, 47);}
-			    }			
+		for(int i=1;i<4;i++){
+		Forms.rectangle(par1, par2-3, par3+i, par4-7, 7, 7, Brick);
+		Forms.rectangle(par1, par2-2, par3+i, par4-6, 5, 5, BookShelf);
 		}
-			
-
-		for(int i = 3;i<=5;i++){
-		    par1.setBlock(par2-1,par3+5,par4-i,108,0,0x01);
-            par1.setBlock(par2+1,par3+5,par4-i,108,1,0x01); 
-		    for(int ii = -1;ii<=1;ii++){
-				par1.setBlock(par2+ii, par3+2, par4-i,0);
-				par1.setBlock(par2+ii, par3+3, par4-i,0);
-				par1.setBlock(par2+ii, par3+1, par4-i,0);
-			}			
+		Forms.square(par1, par2-3, par3, par4-7, 7, 7, Brick);
+		
+		for(int i=0;i<4;i++){
+		Forms.rectangle(par1, par2-4+i, par3+3+i, par4-8+i, 9-2*i, 9-2*i, BrickStair);
 		}
-
+		Forms.rectangle(par1, par2-4, par3, par4-8, 9, 9, BrickStair);
 		
-		par1.setBlock(par2, par3+1, par4-2,0);
-		par1.setBlock(par2, par3+2, par4-2,0);
-		par1.setBlock(par2, par3+1, par4-1,64,3,0x01);
-		par1.setBlock(par2, par3+2, par4-1,64,8,0x01);
-		par1.setBlock(par2, par3+1, par4-4, 116);
+		par1.setBlock(par2, par3+1, par4-2,Air);
+		par1.setBlock(par2, par3+2, par4-2,Air);
 		
-		for(int i =-2;i<=2;i++){
-			par1.setBlock(par2-i,par3+4,par4-2,108,3,0x01);
-			par1.setBlock(par2-i,par3+4,par4-6,108,2,0x01);			
-		}
+		Forms.door(par1, par2, par3+1, par4-1, 2, doorWood);
+				
+		par1.setBlock(par2, par3+1, par4-4, entchantTable);
 		
-		for(int i =-1;i<=1;i++){
-			par1.setBlock(par2-i,par3+5,par4-3,108,3,0x01);
-			par1.setBlock(par2-i,par3+5,par4-5,108,2,0x01);			
-		}
+		par1.setBlock(par2+1, par3, par4+1, BrickStair,1,0x01);
+		par1.setBlock(par2, par3, par4+1, BrickStair,3,0x01);
+		par1.setBlock(par2-1, par3, par4+1, BrickStair,0,0x01);
 		
-		par1.setBlock(par2-1, par3, par4-3,49);
-		par1.setBlock(par2-1, par3, par4-5,49);
-		par1.setBlock(par2+1, par3, par4-3,49);
-		par1.setBlock(par2+1, par3, par4-5,49);
-		par1.setBlock(par2, par3, par4-4,49);
-		par1.setBlock(par2, par3, par4-3,89);
-		par1.setBlock(par2, par3, par4-5,89);
-		par1.setBlock(par2+1, par3, par4-4,89);
-		par1.setBlock(par2-1, par3, par4-4,89);
+		par1.setBlock(par2-1, par3, par4-3,obsidian);
+		par1.setBlock(par2-1, par3, par4-5,obsidian);
+		par1.setBlock(par2+1, par3, par4-3,obsidian);
+		par1.setBlock(par2+1, par3, par4-5,obsidian);
+		par1.setBlock(par2, par3, par4-4,obsidian);
 		
-		par1.setBlock(par2, par3+6, par4-4,44,4,0x01);
+		par1.setBlock(par2, par3, par4-3,glowStone);
+		par1.setBlock(par2, par3, par4-5,glowStone);
+		par1.setBlock(par2+1, par3, par4-4,glowStone);
+		par1.setBlock(par2-1, par3, par4-4,glowStone);
+		
+		par1.setBlock(par2,par3,par4,161);
+		
+		par1.setBlock(par2, par3+7, par4-4,BrickSlab,4,0x01);
 	}
-	
 	
 	public static void south(World par1,int par2,int par3,int par4,EntityPlayer par5){
-		par4 = par4+8;
-		
-		for(int i = -4;i<=4;i++){par1.setBlock(par2+i,par3,par4,108,3,0x01);}
-		
-		for(int i = 1;i<=8;i++){
-		    par1.setBlock(par2+4,par3,par4-i,108,1,0x01);
-		    par1.setBlock(par2-4,par3,par4-i,108,0,0x01);}
-				
-		for(int i = -3;i<=3;i++){par1.setBlock(par2-i,par3,par4-8,108,2,0x01);}
-		
-	
 
-		for(int i = 1;i<=7;i++){
-			par1.setBlock(par2-3,par3+3,par4-i,108,0,0x01);			
-			par1.setBlock(par2+3,par3+3,par4-i,108,1,0x01);
-			for(int ii = -3;ii<=3;ii++){
-			    for(int iii = 0;iii<=2;iii++){par1.setBlock(par2+ii, par3+iii, par4-i, 45);}
-			    }
+
+		for(int i=1;i<4;i++){
+			Forms.rectangle(par1, par2-3, par3+i, par4+1, 7, 7, Brick);
+			Forms.rectangle(par1, par2-2, par3+i, par4+2, 5, 5, BookShelf);
 			}
-		
-		for(int i = 2;i<=6;i++){
-		    par1.setBlock(par2-2,par3+4,par4-i,108,0,0x01);
-			par1.setBlock(par2+2,par3+4,par4-i,108,1,0x01);
-			for(int ii = -2;ii<=2;ii++){
-			    for(int iii = 1;iii<=3;iii++)
-				par1.setBlock(par2+ii, par3+iii, par4-i, 47);
-			}			
-		}
+			Forms.square(par1, par2-3, par3, par4+1, 7, 7, Brick);
 			
-		for(int i = 3;i<=5;i++){
-		    par1.setBlock(par2-1,par3+5,par4-i,108,0,0x01);			
-			par1.setBlock(par2+1,par3+5,par4-i,108,1,0x01);
-			for(int ii = -1;ii<=1;ii++){
-			    for(int iii = 1;iii<=3;iii++){par1.setBlock(par2+ii, par3+iii, par4-i,0);}
-			}			
-		}
+			for(int i=0;i<4;i++){
+			Forms.rectangle(par1, par2-4+i, par3+3+i, par4+i, 9-2*i, 9-2*i, BrickStair);
+			}
+			
+			Forms.rectangle(par1, par2-4, par3, par4, 9, 9, BrickStair);
+			
+		par1.setBlock(par2, par3+1, par4+2,Air);
+		par1.setBlock(par2, par3+2, par4+2,Air);
+		
+		Forms.door(par1, par2, par3+1, par4+1, 0, doorWood);
+		
+		par1.setBlock(par2, par3+1, par4+4, entchantTable);
+		
+		par1.setBlock(par2-1, par3, par4+5,obsidian);
+		par1.setBlock(par2-1, par3, par4+3,obsidian);
+		par1.setBlock(par2+1, par3, par4+5,obsidian);
+		par1.setBlock(par2+1, par3, par4+3,obsidian);
+		par1.setBlock(par2, par3, par4+4,obsidian);
+		
+		par1.setBlock(par2, par3, par4+5,glowStone);
+		par1.setBlock(par2, par3, par4+3,glowStone);
+		par1.setBlock(par2+1, par3, par4+4,glowStone);
+		par1.setBlock(par2-1, par3, par4+4,glowStone);
 
-		par1.setBlock(par2, par3+1, par4-6,0);
-		par1.setBlock(par2, par3+2, par4-6,0);
-		par1.setBlock(par2, par3+1, par4-7,64,6,0x01);
-		par1.setBlock(par2, par3+2, par4-7,64,9,0x01);
-		par1.setBlock(par2, par3+1, par4-4, 116);
+		par1.setBlock(par2+1, par3, par4-1, BrickStair,1,0x01);
+		par1.setBlock(par2, par3, par4-1, BrickStair,2,0x01);
+		par1.setBlock(par2-1, par3, par4-1, BrickStair,0,0x01);
 		
-		par1.setBlock(par2-1, par3, par4-3,49);
-		par1.setBlock(par2-1, par3, par4-5,49);
-		par1.setBlock(par2+1, par3, par4-3,49);
-		par1.setBlock(par2+1, par3, par4-5,49);
-		par1.setBlock(par2, par3, par4-4,49);
-		par1.setBlock(par2, par3, par4-3,89);
-		par1.setBlock(par2, par3, par4-5,89);
-		par1.setBlock(par2+1, par3, par4-4,89);
-		par1.setBlock(par2-1, par3, par4-4,89);
+		par1.setBlock(par2, par3+7, par4+4,BrickSlab,4,0x01);
 		
-		for(int i =-3;i<=3;i++){
-			par1.setBlock(par2-i,par3+3,par4-1,108,3,0x01);
-			par1.setBlock(par2-i,par3+3,par4-7,108,2,0x01);				
+		par1.setBlock(par2,par3,par4,161,0,0x01);
+		
 		}
-						
-		for(int i =-2;i<=2;i++){
-			par1.setBlock(par2-i,par3+4,par4-2,108,3,0x01);		
-			par1.setBlock(par2-i,par3+4,par4-6,108,2,0x01);			
-		}
-		
-		
-		for(int i =-1;i<=1;i++){
-			par1.setBlock(par2-i,par3+5,par4-3,108,3,0x01);			
-			par1.setBlock(par2-i,par3+5,par4-5,108,2,0x01);			
-		}
-		par1.setBlock(par2, par3+6, par4-4,44,4,0x01);
-		
-		par1.setBlock(par2,par3,par4-8,161,0,0x01);
-		par1.setBlock(par2  ,par3,par4-9,108,2,0x01);
-		par1.setBlock(par2+1,par3,par4-9,108,1,0x01);
-		par1.setBlock(par2-1,par3,par4-9,108,0,0x01);
-	}
 	
 	public static void west(World par1,int par2,int par3,int par4,EntityPlayer par5){
-		par4 = par4+8;
+
+		for(int i=1;i<4;i++){
+			Forms.rectangle(par1, par2-7, par3+i, par4-3, 7, 7, Brick);
+			Forms.rectangle(par1, par2-6, par3+i, par4-2, 5, 5, BookShelf);
+			}
+			Forms.square(par1, par2-7, par3, par4-3, 7, 7, Brick);
+			
+			for(int i=0;i<4;i++){
+			Forms.rectangle(par1, par2-8+i, par3+3+i, par4-4+i, 9-2*i, 9-2*i, BrickStair);
+			}
+			
+			Forms.rectangle(par1, par2-8, par3, par4-4, 9, 9, BrickStair);
 		
-		for(int i = -4;i<=4;i++){par1.setBlock(par2+i,par3,par4,108,3,0x01);}
+		par1.setBlock(par2-2, par3+1, par4,Air);
+		par1.setBlock(par2-2, par3+2, par4,Air);
 		
-		for(int i = 1;i<=8;i++){
-		    par1.setBlock(par2+4,par3,par4-i,108,1,0x01);
-		    par1.setBlock(par2-4,par3,par4-i,108,0,0x01);}
-				
-		for(int i = -3;i<=3;i++){par1.setBlock(par2-i,par3,par4-8,108,2,0x01);}
+		Forms.door(par1, par2-1, par3+1, par4, 1, doorWood);
+		
+		par1.setBlock(par2-4, par3+1, par4, entchantTable);
+		
+		par1.setBlock(par2-5, par3, par4+1,obsidian);
+		par1.setBlock(par2-5, par3, par4-1,obsidian);
+		par1.setBlock(par2-3, par3, par4+1,obsidian);
+		par1.setBlock(par2-3, par3, par4-1,obsidian);
+		par1.setBlock(par2-4, par3, par4,obsidian);
+		
+		par1.setBlock(par2-4, par3, par4+1,glowStone);
+		par1.setBlock(par2-4, par3, par4-1,glowStone);
+		par1.setBlock(par2-3, par3, par4,glowStone);
+		par1.setBlock(par2-5, par3, par4,glowStone);
 		
 	
-
-		for(int i = 1;i<=7;i++){
-			par1.setBlock(par2-3,par3+3,par4-i,108,0,0x01);			
-			par1.setBlock(par2+3,par3+3,par4-i,108,1,0x01);
-			for(int ii = -3;ii<=3;ii++){
-			    for(int iii = 0;iii<=2;iii++){par1.setBlock(par2+ii, par3+iii, par4-i, 45);}
-			    }
-			}
+		par1.setBlock(par2-4, par3+7, par4,BrickSlab,4,0x01);
 		
-		for(int i = 2;i<=6;i++){
-		    par1.setBlock(par2-2,par3+4,par4-i,108,0,0x01);
-			par1.setBlock(par2+2,par3+4,par4-i,108,1,0x01);
-			for(int ii = -2;ii<=2;ii++){
-			    for(int iii = 1;iii<=3;iii++)
-				par1.setBlock(par2+ii, par3+iii, par4-i, 47);
-			}			
-		}
-			
-		for(int i = 3;i<=5;i++){
-		    par1.setBlock(par2-1,par3+5,par4-i,108,0,0x01);			
-			par1.setBlock(par2+1,par3+5,par4-i,108,1,0x01);
-			for(int ii = -1;ii<=1;ii++){
-			    for(int iii = 1;iii<=3;iii++){par1.setBlock(par2+ii, par3+iii, par4-i,0);}
-			}			
-		}
-
-		par1.setBlock(par2+2, par3+1, par4-4,0);
-		par1.setBlock(par2+2, par3+2, par4-4,0);                  
-		par1.setBlock(par2+3, par3+1, par4-4,64,2,0x01);          
-		par1.setBlock(par2+3, par3+2, par4-4,64,10,0x01);          
-		par1.setBlock(par2, par3+1, par4-4, 116);               
-		                                                        
-		par1.setBlock(par2-1, par3, par4-3,49);                 
-		par1.setBlock(par2-1, par3, par4-5,49);                 
-		par1.setBlock(par2+1, par3, par4-3,49);                 
-		par1.setBlock(par2+1, par3, par4-5,49);                 
-		par1.setBlock(par2, par3, par4-4,49);                   
-		par1.setBlock(par2, par3, par4-3,89);                   
-		par1.setBlock(par2, par3, par4-5,89);                   
-		par1.setBlock(par2+1, par3, par4-4,89);                 
-		par1.setBlock(par2-1, par3, par4-4,89);
+		par1.setBlock(par2,par3,par4,161,0,0x01);
 		
-		for(int i =-3;i<=3;i++){
-			par1.setBlock(par2-i,par3+3,par4-1,108,3,0x01);
-			par1.setBlock(par2-i,par3+3,par4-7,108,2,0x01);				
-		}
-						
-		for(int i =-2;i<=2;i++){
-			par1.setBlock(par2-i,par3+4,par4-2,108,3,0x01);		
-			par1.setBlock(par2-i,par3+4,par4-6,108,2,0x01);			
-		}
+		par1.setBlock(par2+1,par3,par4+1,BrickStair,3,0x01);
+		par1.setBlock(par2+1,par3,par4,BrickStair,1,0x01);
+		par1.setBlock(par2+1,par3,par4-1,BrickStair,2,0x01);
 		
-		
-		for(int i =-1;i<=1;i++){
-			par1.setBlock(par2-i,par3+5,par4-3,108,3,0x01);			
-			par1.setBlock(par2-i,par3+5,par4-5,108,2,0x01);			
-		}
-		par1.setBlock(par2, par3+6, par4-4,44,4,0x01);
-		
-		par1.setBlock(par2+4,par3,par4-4,161,0,0x01);
-		par1.setBlock(par2+5,par3,par4-3,108,3,0x01);
-		par1.setBlock(par2+5,par3,par4-4,108,1,0x01);
-		par1.setBlock(par2+5,par3,par4-5,108,2,0x01);
-		
-		Forms.rectangle(par1, par2, par3+9, par4, 7, 8, 20);
 	}
 }
