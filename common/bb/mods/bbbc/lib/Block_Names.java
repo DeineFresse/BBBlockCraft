ac364a3c0208b97a0549fbf40045387be4a0e4de
BB20101997 <blessmann-bennet@t-online.de>
2013-08-22 15:40:37 +0200

    Forgot to adde the Block name lib in the last commit so here it is :P

---
+++ common/bb/mods/bbbc/lib/Block_Names.java
@@ -0,0 +1,10 @@
+package bb.mods.bbbc.lib;
+
+public class Block_Names {
+
+	public static final String TESTBLOCK = "Brick House";
+	public static final String BLOCKOREONE= "Block Ore One";
+	public static final String CLOUDORE = "Cloud Ore";
+	public static final String CLOUDWHITE = "Cloud";
+	
+}
