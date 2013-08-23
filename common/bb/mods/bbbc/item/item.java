fdb3f3e9671b20588ee8b3e19a3f7e64b5ff49ca
BB20101997 <blessmann-bennet@t-online.de>
2013-08-23 17:20:14 +0200

    made it usefull

--- common/bb/mods/bbbc/item/item.java
+++ common/bb/mods/bbbc/item/item.java
@@ -1,5 +1,30 @@
 package bb.mods.bbbc.item;
 
-public class item {
+import bb.mods.bbbc.lib.Item_Names;
+import net.minecraft.src.ModLoader;
+import net.minecraft.item.Item;
 
+public class item {
+	
+	
+	  // Block Definitions
+    public static Item  MyFirstItem;
+    public static Item NineBrickStairsItem;
+    public static Item Bookwallitem;
+    
+	public static void signItemID(){
+	  
+	    MyFirstItem = new MyFirstItem(2100);
+	  	NineBrickStairsItem =new NineBrickStairsItem(2101);
+	  	Bookwallitem = new Bookwallitem(2102);
+	    }	
+	 
+	 
+	 public static void Itemname(){
+	     ModLoader.addName(MyFirstItem, Item_Names.MYFIRSTITEM);
+	     ModLoader.addName(NineBrickStairsItem,Item_Names.NBSI);
+	     ModLoader.addName(Bookwallitem,Item_Names.BOOKWALLITEM);
+	   
+	 }
+	 
 }
