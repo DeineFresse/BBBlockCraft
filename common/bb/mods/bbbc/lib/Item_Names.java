05b10bc8ae9bd799a54a5eb191d0afd08121912f
BB20101997 <blessmann-bennet@t-online.de>
2013-08-23 17:17:44 +0200

    The name register for items!

---
+++ common/bb/mods/bbbc/lib/Item_Names.java
@@ -0,0 +1,9 @@
+package bb.mods.bbbc.lib;
+
+public class Item_Names {
+	
+	public static final String MYFIRSTITEM = "My first Item";
+	public static final String NBSI = "Brick Stair Item";
+	public static final String BOOKWALLITEM = "Bookwall";
+
+}
