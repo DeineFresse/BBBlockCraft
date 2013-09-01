825cfd326c5a4b074694382d9fbc13afc64e5c36
BB20101997 <blessmann-bennet@t-online.de>
2013-09-01 20:39:39 +0200

    Changed where the Achievemenrs get added etc.

--- common/bb/mods/bbbc/BBBlockCraft.java
+++ common/bb/mods/bbbc/BBBlockCraft.java
@@ -5,12 +5,9 @@ import bb.mods.bbbc.item.item;
 import bb.mods.bbbc.lib.Reference;
 import bb.mods.bbbc.reciepe.crafting;
 import bb.mods.bbbc.reciepe.smelting;
+import bb.mods.bbbc.stats.Achievements;
 import bb.mods.bbbc.world.gen.BBWorldGen;
 import bb.mods.bbbc.common.CommonProxy;
-import net.minecraft.item.Item;
-import net.minecraft.stats.Achievement;
-import net.minecraft.stats.AchievementList;
-import net.minecraftforge.common.AchievementPage;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
@@ -19,7 +16,6 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
 @NetworkMod(channels = { "BBBlockCraft_alt" }, clientSideRequired = true, serverSideRequired = false, packetHandler = bb.mods.bbbc.common.network.PacketHandler.class)
@@ -32,36 +28,7 @@ public class BBBlockCraft {
 
 	public static bb.mods.bbbc.world.gen.BBWorldGen worldGen = new BBWorldGen();
 
-	// Achievement´s
-
-	public static Achievement firstAchievement = new Achievement(2001,
-			"Getting Ore", 0, 2, Item.diamond, AchievementList.openInventory)
-			.setSpecial().registerAchievement();
-	public static Achievement secondAchievement = new Achievement(2002,
-			"Smelting the Ore", 0, 4, Item.diamond,
-			AchievementList.openInventory).registerAchievement();
-
-	// Achivement´s names
-	private static void addAchievementName(String ach, String name) {
-		LanguageRegistry.instance().addStringLocalization(ach, "en_US", name);
-	}
-
-	private static void addAchievementDesc(String ach, String desc) {
-		LanguageRegistry.instance().addStringLocalization(ach, "en_US", desc);
-	}
-
-	static void addAchievementLocalizations() {
-		BBBlockCraft.addAchievementName("firstAchievement", "Got the Ore!");
-		BBBlockCraft.addAchievementName("secondAchievement", "Smelt it!");
-		BBBlockCraft.addAchievementDesc("firstAchievement", "Get the Ore!");
-		BBBlockCraft.addAchievementDesc("secondAchievement",
-				"You did it right!");
-	}
-
-	// Achievement Page
-	public AchievementPage page1 = new AchievementPage("BBBlockCraft",
-			firstAchievement, secondAchievement);
-
+	
 	public static BBBlockCraft instance;
 
 	@SidedProxy(clientSide = "bb.mods.bbbc.common.ClientProxy", serverSide = "bb.mods.bbbc.common.CommonProxy")
@@ -79,16 +46,13 @@ public class BBBlockCraft {
 	@EventHandler
 	public void init(FMLInitializationEvent evtent) {
 
-		BBBlockCraft.addAchievementLocalizations();
-
-		// Register AchivementPage
-		AchievementPage.registerAchievementPage(page1);
+		Achievements.addAchievementLocalizations();
+		Achievements.addAchievementPage();
 
 		smelting.SmeltingRecipe();
 		crafting.Recipe();
 		crafting.ShapelessRecipe();
 
-		// Add World Gen
 		GameRegistry.registerWorldGenerator(worldGen);
 
 	}
---
+++ common/bb/mods/bbbc/stats/Achievements.java
@@ -0,0 +1,41 @@
+package bb.mods.bbbc.stats;
+
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.stats.Achievement;
+import net.minecraft.stats.AchievementList;
+import net.minecraftforge.common.AchievementPage;
+
+public class Achievements {
+
+	public static Achievement firstAchievement = new Achievement(2001,
+			"Getting Ore", 0, 2, Item.diamond, AchievementList.openInventory)
+			.setSpecial().registerAchievement();
+	public static Achievement secondAchievement = new Achievement(2002,
+			"Smelting the Ore", 2, 2, Item.diamond,
+			firstAchievement).registerAchievement();
+
+	private static void addAchievementName(String ach, String name) {
+		LanguageRegistry.instance().addStringLocalization(ach, "en_US", name);
+	}
+
+	private static void addAchievementDesc(String ach, String desc) {
+		LanguageRegistry.instance().addStringLocalization(ach, "en_US", desc);
+	}
+	
+	public static void addAchievementLocalizations()
+    {
+                    bb.mods.bbbc.stats.Achievements.addAchievementName("firstAchievement", "Got the Ore!");
+                    bb.mods.bbbc.stats.Achievements.addAchievementName("secondAchievement","Smelt it!");
+                    
+                    bb.mods.bbbc.stats.Achievements.addAchievementDesc("firstAchievement", "Get the Ore!");
+                    bb.mods.bbbc.stats.Achievements.addAchievementDesc("secondAchievement","You did it right!" );
+    }
+	
+	public static AchievementPage page1 = new AchievementPage("BBBlockCraft",new Achievement[]{AchievementList.openInventory,firstAchievement,secondAchievement});
+	
+	public static void addAchievementPage(){
+		AchievementPage.registerAchievementPage(page1);
+	}
+	
+}
