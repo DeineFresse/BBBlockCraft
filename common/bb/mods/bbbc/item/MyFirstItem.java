2f14f57721a40bc0b5a630d3fb56c098b0039e01
BB20101997 <blessmann-bennet@t-online.de>
2013-09-15 20:04:03 +0200

    nothing changed exept removing emty lines

--- common/bb/mods/bbbc/item/MyFirstItem.java
+++ common/bb/mods/bbbc/item/MyFirstItem.java
@@ -3,20 +3,16 @@ package bb.mods.bbbc.item;
 import bb.mods.bbbc.lib.Item_Names;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-public class MyFirstItem extends Item{
-	
-	public MyFirstItem(int itemID){
-		
+
+public class MyFirstItem extends Item {
+
+	public MyFirstItem(int itemID) {
+
 		super(itemID);
 		maxStackSize = 64;
 		setCreativeTab(CreativeTabs.tabMisc);
 		setUnlocalizedName(Item_Names.MYFIRSTITEM);
-		
-	
+
 	}
-	
-	
-	
-	
 
 }
