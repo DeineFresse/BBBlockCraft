28aa7091af25751d1c414241678fe3b6faf0a41f
BB20101997 <blessmann-bennet@t-online.de>
2013-11-20 22:57:49 +0100

    Part of the rewrite

--- common/bb/mods/bbbc/block/TestBlock.java
+++ common/bb/mods/bbbc/block/TestBlock.java
@@ -34,10 +34,7 @@ public class TestBlock extends Block {
 	public boolean onBlockActivated(World par1World, int par2, int par3,
 			int par4, EntityPlayer par5EntityPlayer, int par6, float par7,
 			float par8, float par9) {
-		/*Wird öfters benutzt um Funktionen zu testen*/
-		CheckHouse.HouseBuild(par1World, par2, par3, par4, par5EntityPlayer, par6, par7, par8, par9, ID);
-		//bb.mods.bbbc.world.gen.structure.basic.Forms.wall(par1World, par2, par3, par4, 7, 6, 5, 20);
-		
+		CheckHouse.HouseBuild(par1World, par2, par3, par4, par5EntityPlayer, ID);
 		super.onBlockClicked(par1World, par2, par3, par4, par5EntityPlayer);
 		return true;
 
