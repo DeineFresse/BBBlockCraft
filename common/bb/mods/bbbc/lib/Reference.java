8d4723a5975ae5a82b4cafbcb0b437587b298d0b
BB20101997 <blessmann-bennet@t-online.de>
2013-11-20 22:58:23 +0100

    Changed the way the Replacelist works

--- common/bb/mods/bbbc/lib/Reference.java
+++ common/bb/mods/bbbc/lib/Reference.java
@@ -1,5 +1,8 @@
 package bb.mods.bbbc.lib;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Reference {
 
 	// Debug-Mode On/Off
@@ -13,12 +16,42 @@ public class Reference {
 
 	public static final String CHATPREFIX = "[" + MOD_CHANNEL + "]";
 
-	public static final int[] REPLACEIDLIST = new int[] { 0, 20, 78, 31 };
+	private static List<Integer> replaceList = new ArrayList<Integer>();
 
-	public static final String[] SideSufix = new String[] { "top", "bottom",
+	public static int[] getReplaceList() {
+		
+		if(replaceList.size()>=1){
+		int[] a = new int[replaceList.size()];
+		for(int i=0;i<a.length;i++){
+			a[i] = replaceList.get(i);
+		}
+		return a;
+		}
+		else{
+			addToReplaceList(0);
+			return new int[]{0};
+		}
+	}
+	
+	public static void addToReplaceList(int i){
+		if(!replaceList.contains(i)){
+		replaceList.add(i);
+		}
+	}
+	
+	public static void removeFromReplaceList(int i){
+		for(;replaceList.contains(i);){
+			replaceList.remove(replaceList.indexOf(i));
+		}
+	}
+	
+	
+	private static final String[] SideSufix = new String[] { "top", "bottom",
 			"north", "south", "east", "west" };
-
+	/**
+	 * @param par5 side
+	 * **/
 	public static String getSideSufix(int par5) {
-		return null;
+		return par5<SideSufix.length&&par5>=0?SideSufix[par5]:"";
 	}
 }
