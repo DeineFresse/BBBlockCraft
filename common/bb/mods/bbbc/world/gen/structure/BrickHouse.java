fa1c95b47b5c06348e941659e44a36f36d8e7526
BB20101997 <blessmann-bennet@t-online.de>
2013-11-20 23:00:34 +0100

    Part of the rewrite

--- common/bb/mods/bbbc/world/gen/structure/BrickHouse.java
+++ common/bb/mods/bbbc/world/gen/structure/BrickHouse.java
@@ -7,7 +7,8 @@ import net.minecraft.world.World;
 
 public class BrickHouse {
 
-	static int[] BrickHouse = new int[]{45,108,0,47,64,71,49,89,116,44,4};
+	static int[] BrickHouse = new int[] { 45, 108, 0, 47, 64, 71, 49, 89, 116,
+			44, 4 };
 	static int Brick = 45; // 45
 	static int BrickStair = 108; // 108
 	static int Air = 0;
@@ -23,42 +24,52 @@ public class BrickHouse {
 	static int[] Material;
 
 	public static void BuildBrickHouse(World par1, int par2, int par3,
-			int par4, EntityPlayer par5, int f,int ID) {
-		
+			int par4, EntityPlayer par5, int f, int ID) {
+
 		Material = BrickHouse;
 
+		/*Structure s = new Structure();
+		s.addWall(-3, 0, 1, 7, 7, 4, Brick);
+		s.addWall(-2, 0, 2, 5, 5, 4, BookShelf);
+		for (int i = 0; i < 4; i++) {
+			s.addWall(-4 + i, 3 + i, i, 9 - 2 * i, 9 - 2 * i, 2, BrickStair);
+		}
+		s.addWall(-4, 0, 0, 9, 9, 2, BrickStair);
+		s.addDoor(0, 1, 1, doorWood, f);
+		s.addPlane(-3, 0, 1, 7, 7, Brick);
+		s.addBlock(0, 1, 2, Air);
+		s.addBlock(0, 2, 2, Air);
+		s.addBlock(0, 1, 4, entchantTable);
+		s.addBlock(1, 0, -1, BrickStair, 1);
+		s.addBlock(0, 0, -1, BrickStair, 2);
+		s.addBlock(-1, 0, -1, BrickStair, 0);
+		s.addBlock(-1, 0, 3, obsidian);
+		s.addBlock(-1, 0, 5, obsidian);
+		s.addBlock(1, 0, 3, obsidian);
+		s.addBlock(1, 0, 5, obsidian);
+		s.addBlock(0, 0, 4, obsidian);
+		s.addBlock(0, 0, 3, glowStone);
+		s.addBlock(0, 0, 5, glowStone);
+		s.addBlock(1, 0, 4, glowStone);
+		s.addBlock(-1, 0, 4, glowStone);
+		s.addBlock(0, 0, 0, ID);
+		s.addBlock(0, 0, 4, BrickSlab, SlabMeta);
+*/
 		if (!Reference.DEBUGMODE) {
-			switch (f) {
-			case 0: {
-				build(par1, par2, par3, par4, par5,f,ID);
-				break;
-			}
-			case 1: {
-				build(par1, par2, par3, par4, par5,f,ID);
-				break;
-			}
-			case 2: {
-				build(par1, par2, par3, par4, par5,f,ID);
-				break;
-			}
-			case 3: {
-				build(par1, par2, par3, par4, par5,f,ID);
-				break;
-			}
-			}
-
+			//build2(par1, par2, par3, par4, f, s);
+			 build(par1, par2, par3, par4, par5, f, ID);
 		}
 
 	}
-
-	public static void build(World par1, int par2, int par3, int par4,
-			EntityPlayer par5, int o,int ID) {
+	
+	private static void build(World par1, int par2, int par3, int par4,
+			EntityPlayer par5, int o, int ID) {
 
 		int[] xr = new int[4], xp = new int[17];
 		int xs = 0, xd = 0;
 
-		int[] stair = new int[]{0,0,0};
-		
+		int[] stair = new int[] { 0, 0, 0 };
+
 		int[] zr = new int[4], zp = new int[17];
 		int zs = 0, zd = 0;
 
@@ -76,88 +87,101 @@ public class BrickHouse {
 		}
 
 		switch (o) {
-		case (0):{
+		case (0): {
 			xr = new int[] { -3, -2, -4, -4 };
-			xp = new int[] { 0, 0, 0, 1, 0, -1, -1, -1, 1, 1, 0, 0, 0, 1, -1,0, 0 };
+			xp = new int[] { 0, 0, 0, 1, 0, -1, -1, -1, 1, 1, 0, 0, 0, 1, -1,
+					0, 0 };
 			zr = new int[] { 1, 2, 0, 0 };
-			zp = new int[] { 2, 2, 4, -1, -1,- 1, 3, 5, 3, 5, 4, 3, 5,4, 4, 0, 4 };
+			zp = new int[] { 2, 2, 4, -1, -1, -1, 3, 5, 3, 5, 4, 3, 5, 4, 4, 0,
+					4 };
 			zs = 1;
 			zd = 1;
 			xs = -3;
 			xd = 0;
-			stair = new int[]{1,2,0};
+			stair = new int[] { 1, 2, 0 };
 			break;
 		}
-		case (1):{
+		case (1): {
 			xr = new int[] { -7, -6, -8, -8 };
-			xp = new int[] { -2, -2, -4, 1, 1, 1, -5, -5, -3, -3, -4, -4, -4, -3, -5, 0, -4 };
-			zr = new int[] {-3,-2,-4,-4};
-			zp = new int[] { 0,0,0,1,0,-1,1,-1,1,-1,0,1,-1,0,0,0,0};
+			xp = new int[] { -2, -2, -4, 1, 1, 1, -5, -5, -3, -3, -4, -4, -4,
+					-3, -5, 0, -4 };
+			zr = new int[] { -3, -2, -4, -4 };
+			zp = new int[] { 0, 0, 0, 1, 0, -1, 1, -1, 1, -1, 0, 1, -1, 0, 0,
+					0, 0 };
 			zs = -3;
 			zd = 0;
 			xs = -7;
 			xd = -1;
-			stair = new int[]{3,1,2};
+			stair = new int[] { 3, 1, 2 };
 			break;
 		}
 		case (2): {
 			xr = new int[] { -3, -2, -4, -4 };
-			xp = new int[] { 0, 0, 0, 1, 0, -1, -1, -1, 1, 1, 0, 0, 0, 1, -1,0, 0 };
+			xp = new int[] { 0, 0, 0, 1, 0, -1, -1, -1, 1, 1, 0, 0, 0, 1, -1,
+					0, 0 };
 			zr = new int[] { -7, -6, -8, -8 };
-			zp = new int[] { -2, -2, -4, 1, 1, 1, -3, -5, -3, -5, -4, -3, -5,-4, -4, 0, -4 };
+			zp = new int[] { -2, -2, -4, 1, 1, 1, -3, -5, -3, -5, -4, -3, -5,
+					-4, -4, 0, -4 };
 			zs = -7;
 			zd = -1;
 			xs = -3;
 			xd = 0;
-			stair = new int[]{1,3,0};
+			stair = new int[] { 1, 3, 0 };
 			break;
 		}
-		case (3):{
-			xr = new int[] { 1,2,0,0 };
-			xp = new int[] { 2,2,4,-1,-1,-1,3,3,5,5,4,4,4,5,3,0,4,1 };
-			zr = new int[] { -3,-2,-4,-4};
-			zp = new int[] { 0,0,0,1,0,-1,1,-1,1,-1,0,1,-1,0,0,0,0};
+		case (3): {
+			xr = new int[] { 1, 2, 0, 0 };
+			xp = new int[] { 2, 2, 4, -1, -1, -1, 3, 3, 5, 5, 4, 4, 4, 5, 3, 0,
+					4, 1 };
+			zr = new int[] { -3, -2, -4, -4 };
+			zp = new int[] { 0, 0, 0, 1, 0, -1, 1, -1, 1, -1, 0, 1, -1, 0, 0,
+					0, 0 };
 			zs = -3;
 			zd = 0;
 			xs = 1;
 			xd = 1;
-			stair = new int[]{3,0,2};
+			stair = new int[] { 3, 0, 2 };
 			break;
 		}
 		}
 
 		for (int i = 1; i < 4; i++) {
-			Forms.rectangle(par1, par2 + xr[0], par3 + i, par4 + zr[0], 7, 7,
-					Brick);
-			Forms.rectangle(par1, par2 + xr[1], par3 + i, par4 + zr[1], 5, 5,
-					BookShelf);
+			Forms.rectanglehollow(par1, par2 + xr[0], par3 + i, par4 + zr[0],
+					7, 7, Brick);
+			Forms.rectanglehollow(par1, par2 + xr[1], par3 + i, par4 + zr[1],
+					5, 5, BookShelf);
 		}
 
-		Forms.square(par1, par2 + xs, par3, par4 + zs, 7, 7, Brick);
+		Forms.rectanglefull(par1, par2 + xs, par3, par4 + zs, 7, 7, Brick);
 
 		for (int i = 0; i < 4; i++) {
-			Forms.rectangle(par1, par2 + xr[2] + i, par3 + 3 + i, par4 + zr[2]
-					+ i, 9 - 2 * i, 9 - 2 * i, BrickStair);
+			Forms.rectanglehollow(par1, par2 + xr[2] + i, par3 + 3 + i, par4
+					+ zr[2] + i, 9 - 2 * i, 9 - 2 * i, BrickStair);
 		}
 
-		Forms.rectangle(par1, par2 + xr[3], par3, par4 + zr[3], 9, 9,BrickStair);
-		par1.setBlock(par2 + xp[0],  par3 + 1, par4 + zp[0],  Air);
-		par1.setBlock(par2 + xp[1],  par3 + 2, par4 + zp[1],  Air);
-		par1.setBlock(par2 + xp[2],  par3 + 1, par4 + zp[2],  entchantTable);
-		par1.setBlock(par2 + xp[3],  par3,     par4 + zp[3],  BrickStair, stair[0], 0x01);
-		par1.setBlock(par2 + xp[4],  par3,     par4 + zp[4],  BrickStair, stair[1], 0x01);
-		par1.setBlock(par2 + xp[5],  par3,     par4 + zp[5],  BrickStair, stair[2], 0x01);
-		par1.setBlock(par2 + xp[6],  par3,     par4 + zp[6],  obsidian);
-		par1.setBlock(par2 + xp[7],  par3,     par4 + zp[7],  obsidian);
-		par1.setBlock(par2 + xp[8],  par3,     par4 + zp[8],  obsidian);
-		par1.setBlock(par2 + xp[9],  par3,     par4 + zp[9],  obsidian);
-		par1.setBlock(par2 + xp[10], par3,     par4 + zp[10], obsidian);
-		par1.setBlock(par2 + xp[11], par3,     par4 + zp[11], glowStone);
-		par1.setBlock(par2 + xp[12], par3,     par4 + zp[12], glowStone);
-		par1.setBlock(par2 + xp[13], par3,     par4 + zp[13], glowStone);
-		par1.setBlock(par2 + xp[14], par3,     par4 + zp[14], glowStone);
-		par1.setBlock(par2 + xp[15], par3,     par4 + zp[15], ID);
-		par1.setBlock(par2 + xp[16], par3 + 7, par4 + zp[16], BrickSlab,SlabMeta, 0x01);
-		Forms.door(par1, par2+xd, par3 + 1, par4 +zd, o, doorWood);
+		Forms.rectanglehollow(par1, par2 + xr[3], par3, par4 + zr[3], 9, 9,
+				BrickStair);
+		par1.setBlock(par2 + xp[0], par3 + 1, par4 + zp[0], Air);
+		par1.setBlock(par2 + xp[1], par3 + 2, par4 + zp[1], Air);
+		par1.setBlock(par2 + xp[2], par3 + 1, par4 + zp[2], entchantTable);
+		par1.setBlock(par2 + xp[3], par3, par4 + zp[3], BrickStair, stair[0],
+				3);
+		par1.setBlock(par2 + xp[4], par3, par4 + zp[4], BrickStair, stair[1],
+				3);
+		par1.setBlock(par2 + xp[5], par3, par4 + zp[5], BrickStair, stair[2],
+				3);
+		par1.setBlock(par2 + xp[6], par3, par4 + zp[6], obsidian);
+		par1.setBlock(par2 + xp[7], par3, par4 + zp[7], obsidian);
+		par1.setBlock(par2 + xp[8], par3, par4 + zp[8], obsidian);
+		par1.setBlock(par2 + xp[9], par3, par4 + zp[9], obsidian);
+		par1.setBlock(par2 + xp[10], par3, par4 + zp[10], obsidian);
+		par1.setBlock(par2 + xp[11], par3, par4 + zp[11], glowStone);
+		par1.setBlock(par2 + xp[12], par3, par4 + zp[12], glowStone);
+		par1.setBlock(par2 + xp[13], par3, par4 + zp[13], glowStone);
+		par1.setBlock(par2 + xp[14], par3, par4 + zp[14], glowStone);
+		par1.setBlock(par2 + xp[15], par3, par4 + zp[15], ID);
+		par1.setBlock(par2 + xp[16], par3 + 7, par4 + zp[16], BrickSlab,
+				SlabMeta, 3);
+		Forms.door(par1, par2 + xd, par3 + 1, par4 + zd, o, doorWood);
 	}
 }
