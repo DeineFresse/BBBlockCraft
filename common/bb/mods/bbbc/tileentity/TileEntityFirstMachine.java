36af9f70b29027f0e94296fff77c7d4605170aaf
BB20101997 <blessmann-bennet@t-online.de>
2013-11-20 22:57:28 +0100

    As I said earlier here comes the change

--- common/bb/mods/bbbc/tileentity/TileEntityFirstMachine.java
+++ common/bb/mods/bbbc/tileentity/TileEntityFirstMachine.java
@@ -1,79 +1,24 @@
 package bb.mods.bbbc.tileentity;
 
 import net.minecraft.block.Block;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
-import net.minecraft.tileentity.TileEntity;
 
-public class TileEntityFirstMachine extends TileEntity implements IInventory{
-
-	private ItemStack[] items;
+public class TileEntityFirstMachine extends TileEntityInventoryBB {
 	
 	public TileEntityFirstMachine(){
 		items = new ItemStack[4];
+		valid = new int[items.length][1];
+		for(int i = 0;i<valid.length;i++){
+			valid[i][0] = Block.anvil.blockID;
+		}
 		
 	}
 	
 	@Override
-	public int getSizeInventory() {
-		return items.length;
-	}
-
-	@Override
-	public ItemStack getStackInSlot(int i) {
-		return items[i];
-	}
-
-	@Override
-	public ItemStack decrStackSize(int i, int j) {
-		ItemStack itemstack = getStackInSlot(i);
-		
-		if(itemstack != null){
-			if(itemstack.stackSize <= j){
-				setInventorySlotContents(i,null);
-			}
-			else{
-				itemstack = itemstack.splitStack(j);
-				onInventoryChanged();
-			}
-		}
-				return itemstack;
-	}
-
-	@Override
-	public ItemStack getStackInSlotOnClosing(int i) {
-		ItemStack item = getStackInSlot(i);
-		setInventorySlotContents(i,null);
-		return item;
-	}
-
-	@Override
-	public void setInventorySlotContents(int i, ItemStack itemstack) {
-		items[i] = itemstack;
-		
-		if(itemstack != null && itemstack.stackSize > getInventoryStackLimit()){
-			itemstack.stackSize = getInventoryStackLimit();
-		}
-		
-		onInventoryChanged();		
-	}
-
-	@Override
 	public String getInvName() {
 		return "First Machine";
-	}
-
-	@Override
-	public boolean isInvNameLocalized() {
-		return false;
-	}
-
-	@Override
-	public int getInventoryStackLimit() {
-		return 64;
 	}
 
 	@Override
@@ -83,28 +28,12 @@ public class TileEntityFirstMachine extends TileEntity implements IInventory{
 		anvils = -1;
 	}
 
-	@Override
-	public boolean isUseableByPlayer(EntityPlayer entityplayer) {
-		return entityplayer.getDistanceSq(xCoord + 0.5, yCoord + 0.5, zCoord + 0.5) <=64;
-	}
-
-	@Override
-	public void openChest() {}
-
-	@Override
-	public void closeChest() {}
-
-	@Override
-	public boolean isItemValidForSlot(int i, ItemStack itemstack) {
-		return itemstack.itemID == Block.anvil.blockID;
-	}
 
 	@Override
 	public void writeToNBT(NBTTagCompound compound){
 		super.writeToNBT(compound);
 		
 		NBTTagList items = new NBTTagList();
-	
 		for (int i = 0;i<getSizeInventory();i++){
 			ItemStack stack = getStackInSlot(i);
 			
@@ -118,7 +47,7 @@ public class TileEntityFirstMachine extends TileEntity implements IInventory{
 		compound.setTag("Items", items);
 	}
 	
-	@Override
+ 	@Override
 	public void readFromNBT(NBTTagCompound compound){
 		super.readFromNBT(compound);
 		
@@ -174,5 +103,4 @@ public class TileEntityFirstMachine extends TileEntity implements IInventory{
 			}
 		}
 	}
-	
 }
--- common/bb/mods/bbbc/tileentity/TileEntityHouseMachine.java
+++ common/bb/mods/bbbc/tileentity/TileEntityHouseMachine.java
@@ -1,20 +1,13 @@
 package bb.mods.bbbc.tileentity;
 
-import net.minecraft.block.Block;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
-import net.minecraft.tileentity.TileEntity;
 
-public class TileEntityHouseMachine extends TileEntity implements IInventory{
-
-	private ItemStack[] items;
+public class TileEntityHouseMachine extends TileEntityInventoryBB {
+	
 	private int proccesTimer;
 	private final int MAXTIMER = 200;
-	
-	private int[][] valid;
 	
 	private boolean inventoryChange = true;
 	
@@ -28,10 +21,6 @@ public class TileEntityHouseMachine extends TileEntity implements IInventory{
 		
 	}
 	
-	public int[] getValidItems(int slot){
-		return valid[slot];
-	}
-	
 	public boolean hasInventoryChanged(){
 		return inventoryChange;
 	}
@@ -43,64 +32,10 @@ public class TileEntityHouseMachine extends TileEntity implements IInventory{
 	public int getTimer(){
 		return proccesTimer;
 	}
-	
-	@Override
-	public int getSizeInventory() {
-		return items.length;
-	}
-
-	@Override
-	public ItemStack getStackInSlot(int i) {
-		return items[i];
-	}
-
-	@Override
-	public ItemStack decrStackSize(int i, int j) {
-		ItemStack itemstack = getStackInSlot(i);
-		
-		if(itemstack != null){
-			if(itemstack.stackSize <= j){
-				setInventorySlotContents(i,null);
-			}
-			else{
-				itemstack = itemstack.splitStack(j);
-				onInventoryChanged();
-			}
-		}
-				return itemstack;
-	}
-
-	@Override
-	public ItemStack getStackInSlotOnClosing(int i) {
-		ItemStack item = getStackInSlot(i);
-		setInventorySlotContents(i,null);
-		return item;
-	}
-
-	@Override
-	public void setInventorySlotContents(int i, ItemStack itemstack) {
-		items[i] = itemstack;
-		
-		if(itemstack != null && itemstack.stackSize > getInventoryStackLimit()){
-			itemstack.stackSize = getInventoryStackLimit();
-		}
-		
-		onInventoryChanged();		
-	}
 
 	@Override
 	public String getInvName() {
 		return "First Machine";
-	}
-
-	@Override
-	public boolean isInvNameLocalized() {
-		return false;
-	}
-
-	@Override
-	public int getInventoryStackLimit() {
-		return 64;
 	}
 
 	@Override
@@ -110,23 +45,6 @@ public class TileEntityHouseMachine extends TileEntity implements IInventory{
 		inventoryChange = true;
 	}
 
-	@Override
-	public boolean isUseableByPlayer(EntityPlayer entityplayer) {
-		return entityplayer.getDistanceSq(xCoord + 0.5, yCoord + 0.5, zCoord + 0.5) <=64;
-	}
-
-	@Override
-	public void openChest() {}
-
-	@Override
-	public void closeChest() {}
-
-	@Override
-	public boolean isItemValidForSlot(int i, ItemStack itemstack) {
-		
-		//TODO : Make the right items be accepted
-		return itemstack.itemID == Block.anvil.blockID;
-	}
 
 	@Override
 	public void writeToNBT(NBTTagCompound compound){
@@ -187,5 +105,4 @@ public class TileEntityHouseMachine extends TileEntity implements IInventory{
 		 }
 		}		
 	}
-
 }
