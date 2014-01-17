8fb8f52f138f632f334575983fe38d66c8666f25
BB20101997 <blessmann-bennet@t-online.de>
2014-01-17 14:43:46 +0100

    added growing of teh fishe

--- common/bb/mods/bbbc/unrelated/tileentity/Fishes.java
+++ common/bb/mods/bbbc/unrelated/tileentity/Fishes.java
@@ -8,6 +8,7 @@ public class Fishes {
 
 		public float xCoord, yCoord, zCoord;
 		public float xMove, yMove, zMove;
+		public float size;
 		
 		public Fishes() {
 			Random r = new Random();
@@ -17,22 +18,27 @@ public class Fishes {
 			xMove = r.nextFloat()/200;
 			yMove = r.nextFloat()/200;
 			zMove = r.nextFloat()/200;
+			size = r.nextFloat();
+			if(size<0.1F){
+				size = 0.1F;
+			}
 		}
 
-		public Fishes(float x, float y, float z, float xm, float ym, float zm) {
+		public Fishes(float x, float y, float z, float xm, float ym, float zm,float s) {
 			xCoord = x;
 			yCoord = y;
 			zCoord = z;
 			xMove = xm;
 			yMove = ym;
 			zMove = zm;
+			size = s;
 		}
 
 		public static Fishes fishesFromNBTTagCompound(NBTTagCompound nbttag) {
 			return new Fishes(nbttag.getFloat("xCoord"),
 					nbttag.getFloat("yCoord"), nbttag.getFloat("zCoord"),
 					nbttag.getFloat("xMove"), nbttag.getFloat("yMove"),
-					nbttag.getFloat("zMove"));
+					nbttag.getFloat("zMove"),0.75F);
 		}
 		
 		public void moveFish(){
@@ -50,6 +56,13 @@ public class Fishes {
 			xCoord+=xMove;
 			yCoord+=yMove;
 			zCoord+=zMove;
+			
+			if(0.5F>size){
+				size+=0.000001F;
+			}
+			if(0.5F<size){
+				size = 0.5F;
+			}
 			
 		}
 		
