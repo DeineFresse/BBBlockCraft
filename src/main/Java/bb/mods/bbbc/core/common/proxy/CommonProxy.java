649e841f04fb715ab50ce3f06a38ec04f95caea1
BB20101997 <blessmann-bennet@t-online.de>
2014-10-29 20:05:18 +0100

    added missing stuff

--- src/main/Java/bb/mods/bbbc/core/common/proxy/CommonProxy.java
+++ src/main/Java/bb/mods/bbbc/core/common/proxy/CommonProxy.java
@@ -1,7 +1,12 @@
 package bb.mods.bbbc.core.common.proxy;
 
+import cpw.mods.fml.common.SidedProxy;
+
 /**
  * Created by BB20101997 on 13.08.2014.
  */
-public class CommonProxy {
+public abstract class CommonProxy implements IProxy {
+
+    @SidedProxy(clientSide = "bb.mods.bbbc.core.common.proxy.ClientProxy", serverSide = "bb.mods.bbbc.core.common.proxy.ServerProxy")
+    public static IProxy proxy;
 }
