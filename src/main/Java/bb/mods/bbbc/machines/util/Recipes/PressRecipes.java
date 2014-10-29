c66b7b834bc560ec5906ed496d48a927a21ad85f
BB20101997 <blessmann-bennet@t-online.de>
2014-10-29 20:29:26 +0100

    added Registration to the RecipeRegistry need to look at it again it complains for true and false !BUG!

--- src/main/Java/bb/mods/bbbc/machines/util/Recipes/PressRecipes.java
+++ src/main/Java/bb/mods/bbbc/machines/util/Recipes/PressRecipes.java
@@ -1,6 +1,9 @@
 package bb.mods.bbbc.machines.util.Recipes;
 
+import bb.mods.bbbc.core.util.LogHelper;
 import bb.mods.bbbc.core.util.Recipes.IRecipeBank;
+import bb.mods.bbbc.core.util.Recipes.RecipeRegistry;
+import bb.mods.bbbc.machines.references.RecipeIdentifier;
 
 /**
  * Created by BB20101997 on 11.08.2014.
@@ -9,17 +12,23 @@ public class PressRecipes extends IRecipeBank<PressRecipe> {
 
     public static PressRecipes INSTANCE;
 
+    private PressRecipes(){
+        super(PressRecipe.class);
+    }
+
     public static IRecipeBank getInstance() {
 
         if (INSTANCE == null) {
             INSTANCE = new PressRecipes();
+
+            RecipeRegistry.getInstance().registerBank(RecipeIdentifier.PRESSBANK, INSTANCE);
+            if (RecipeRegistry.getInstance().getRecipeBank(RecipeIdentifier.PRESSBANK) != INSTANCE) {
+                LogHelper.error("Could not register RecipeBank PressRecipe already used identifier!");
+            } else {
+                LogHelper.info("RecipeBank PressRecipe instance already Registered,please check!");
+            }
         }
         return INSTANCE;
-
-    }
-
-    @Override
-    public void addRecipe(PressRecipe irec) {
 
     }
 }
