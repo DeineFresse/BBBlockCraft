e4388dd9b86af53096bcee45e961730a146e5d0b
BB20101997 <blessmann-bennet@t-online.de>
2014-10-29 20:20:50 +0100

    adding a ReciepHandler,and Reciepe storage

--- src/main/Java/bb/mods/bbbc/core/util/Recipes/IRecipeBank.java
+++ src/main/Java/bb/mods/bbbc/core/util/Recipes/IRecipeBank.java
@@ -1,7 +1,43 @@
 package bb.mods.bbbc.core.util.Recipes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by BB20101997 on 13.08.2014.
  */
-public interface IRecipeBank {
+
+public abstract class IRecipeBank<T extends IRecipe> {
+
+    public final List<T> recipeList = new ArrayList<T>();
+
+    public final Class<T> Tclass;
+
+    public IRecipeBank(Class<T> cl){
+        Tclass = cl;
+    }
+
+    public void addRecipe(T irec){
+
+        recipeList.add(irec);
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public T[] getRecipeList() {
+
+        T[] arr = null;
+
+        try{
+
+            arr = recipeList.toArray((T[]) java.lang.reflect.Array.newInstance(Tclass, 0));
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return arr;
+
+    }
+
 }
--- src/main/Java/bb/mods/bbbc/core/util/Recipes/RecipeRegistry.java
+++ src/main/Java/bb/mods/bbbc/core/util/Recipes/RecipeRegistry.java
@@ -1,7 +1,35 @@
 package bb.mods.bbbc.core.util.Recipes;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by BB20101997 on 11.08.2014.
  */
 public class RecipeRegistry {
+
+    private static RecipeRegistry INSTANCE;
+
+    private final Map<String,IRecipeBank> BankMap = new HashMap<String,IRecipeBank>();
+
+    private RecipeRegistry(){}
+
+    public static RecipeRegistry getInstance(){
+
+        if(INSTANCE == null){
+            INSTANCE =new RecipeRegistry();
+        }
+
+        return INSTANCE;
+    }
+    public void registerBank(String pressbank, IRecipeBank instance) {
+            if((!BankMap.containsKey(pressbank))||BankMap.get(pressbank)==null) {
+                BankMap.put(pressbank, instance);
+            }
+    }
+    public IRecipeBank getRecipeBank(String identifier){
+        return BankMap.get(identifier);
+    }
+
+
 }
