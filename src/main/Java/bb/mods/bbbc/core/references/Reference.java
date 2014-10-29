c6e6ee93d64f87e664faf0dba227c42caa4083b2
BB20101997 <blessmann-bennet@t-online.de>
2014-10-29 20:24:10 +0100

    removed due to unused

--- src/main/Java/bb/mods/bbbc/core/references/Reference.java
+++ src/main/Java/bb/mods/bbbc/core/references/Reference.java
@@ -6,20 +6,12 @@ public class Reference {
 	public static final boolean DEBUGMODE = false;
 
 	// General Mod Konstanten
-	public static final String MOD_ID = "BBBlockCraftCore";
-	public static final String MOD_NAME = "BBBlockCraftCore";
+	public static final String MOD_ID = "BBBlockCraft-Core";
+	public static final String MOD_NAME = "BBBlockCraft-Core";
 	public static final String MOD_VERSION = "DEV-0.1.1";
 	public static final String MOD_CHANNEL = "BB_BBBCCore";
-	public static final String MOD_RESOURCE_LOC = "BBBlockCraft";
+	public static final String MOD_RESOURCE_LOC = "bbblockcraft";
 
 	public static final String CHATPREFIX = "[" + MOD_CHANNEL + "]";	
-	
-	private static final String[] SideSufix = new String[] { "top", "bottom",
-			"north", "south", "east", "west" };
-	/**
-	 * @param par5 side
-	 * **/
-	public static String getSideSufix(int par5) {
-		return par5<SideSufix.length&&par5>=0?SideSufix[par5]:null;
-	}
+
 }
